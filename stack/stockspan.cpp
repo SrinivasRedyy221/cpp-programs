4340ee3c1bba520a35dc0abfbf6eb6a24cecf6a7
Srinivas <piserla.srinivas@gmail.com>
2023-08-08 02:35:31 +0530

    updated

---
+++ stack/stockspan.cpp
@@ -0,0 +1,16 @@
+/*Implementation of stock span problem using this stl stack 
+where the stock span is nothing but the count of the consecutive days including the present day wehre the stock price is lesser or equal to
+13,15,12,14,16,8,6,4,10,30
+1, 2, 1, 2, 5, 1 ,1,1, 4,10
+when the array is sorted in the reverse order then the span is always 1*/
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    int arr[]={13,15,12,14,16,8,6,4,10,30};
+    
+
+    return 0;
+}
