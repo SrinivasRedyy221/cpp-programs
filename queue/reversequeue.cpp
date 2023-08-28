c62be63cbaf77e1778a9834902a476ff056eba12
Srinivas <piserla.srinivas@gmail.com>
2023-08-28 22:34:34 +0530

    updated

---
+++ queue/reversequeue.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+void reverse(queue<int> &q){
+    if(q.empty()){
+        return ;
+    }
+    int x=q.front();
+    q.pop();
+    reverse(q);
+    q.push(x);
+}
+
+
+int main(){
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    reverse(q);
+    while(!q.empty()){
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+    cout<<endl;
+    
+    return 0;
+
+}
